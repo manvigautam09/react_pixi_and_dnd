b5aa13d9aacb77bb06d12bb7ae47aea143640c6f
manvigautam09 <mnvsgautam9@gmail.com>
2024-02-21 16:56:06 +0530

    Add DndProvider for drag and drop functionality

--- app/video_maker/page.tsx
+++ app/video_maker/page.tsx
@@ -3,18 +3,22 @@
 import ReelCanvas from "@/components/VideoMaker";
 import { AppProvider } from "@pixi/react";
 import { Application } from "pixi.js";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 const app = new Application();
 
 const VideoMakerApp = () => {
   return (
-    <AppProvider value={app}>
-      <div className="flex justify-center items-center h-screen bg-[#FBFCFE]">
-        <div className="w-full sm:w-96 bg-gray-light-5 h-4/5">
-          <ReelCanvas />
+    <DndProvider backend={HTML5Backend}>
+      <AppProvider value={app}>
+        <div className="flex justify-center items-center h-screen bg-[#FBFCFE]">
+          <div className="w-full sm:w-96 bg-gray-light-5 h-4/5">
+            <ReelCanvas />
+          </div>
         </div>
-      </div>
-    </AppProvider>
+      </AppProvider>
+    </DndProvider>
   );
 };
 

0c60e46a6f9971133c623ff65bc4eca4ad484f7c
manvigautam09 <mnvsgautam9@gmail.com>
2024-02-28 12:48:21 +0530

    Add PIXI.js and fetch slide data***

--- app/test_app/page.tsx
+++ app/test_app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 // Test URL ?duration=2?fps=24?videoId=d2d4c589-ee23-4dc5-a218-fe738e52cd6a
-import React, { Fragment, useRef, useState } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
+import * as PIXI from "pixi.js";
 import { v4 as uuidv4 } from "uuid";
-import { Stage, Text } from "@pixi/react";
-import { AppProvider } from "@pixi/react";
 import { Application } from "pixi.js";
+import { AppProvider, Sprite, Stage, Text } from "@pixi/react";
+
+import { SlideType } from "@/utils/types";
+import { fetchSlideData } from "@/services/helpers";
 
 const app = new Application();
 
@@ -13,6 +16,20 @@ const App = () => {
   const framesData = useRef<any>({});
   const durationRef = useRef(1);
   const durationInsideSecondRef = useRef(1);
+  const [slideData, setSlideData] = useState<SlideType>();
+
+  const getSlideData = async () => {
+    const data: SlideType = await fetchSlideData();
+    if (data !== undefined) {
+      setSlideData(data);
+    }
+  };
+
+  useEffect(() => {
+    if (!slideData) {
+      getSlideData();
+    }
+  }, [slideData]);
 
   const [videoDuration, setVideoDuration] = useState(
     location.search.split("?").length > 1
@@ -114,6 +131,9 @@ const App = () => {
     }
   };
 
+  const slideType = slideData?.type;
+  const text = slideData?.text;
+
   return (
     <AppProvider value={app}>
       <Fragment>
@@ -139,7 +159,7 @@ const App = () => {
           <input
             type="number"
             min={2}
-            max={30}
+            max={300}
             value={videoDuration}
             disabled={recordingVideo}
             onChange={handleInputChange}
@@ -165,12 +185,53 @@ const App = () => {
             Download
           </button>
         </div>
-        <div style={{ position: "relative" }}>
-          <Stage ref={stageRef} options={{ backgroundColor: "#1099bb" }}>
-            {/* <BunnyAnimation /> */}
-            <Text text="Loading.." />
-          </Stage>
-        </div>
+        <Stage
+          ref={stageRef}
+          options={{ backgroundColor: "#EAECF0EE" }}
+          width={400}
+          height={700}
+        >
+          {slideType === "image" && (
+            <Sprite
+              image={`http://localhost:3005/slide-image?name=${slideData?.asset}`}
+              width={400}
+              height={700}
+            />
+          )}
+
+          {text &&
+            text?.length > 0 &&
+            text.map((t) => (
+              <Text
+                key={t.id}
+                text={t.title}
+                x={t.position?.x || 100}
+                y={t.position?.y || 100}
+                style={
+                  new PIXI.TextStyle({
+                    align: t.style?.align || "center",
+                    fontFamily:
+                      t.style?.fontFamily ||
+                      '"Source Sans Pro", Helvetica, sans-serif',
+                    fontSize: t.style?.fontSize || 20,
+                    fontWeight: t.style?.fontWeight || "400",
+                    fill: t.style?.fill || "#000", // can also add gradient
+                    stroke: t.style?.stroke || "#000",
+                    strokeThickness: t.style?.strokeThickness || 0,
+                    letterSpacing: t.style?.letterSpacing || 1,
+                    dropShadow: t.style?.dropShadow || false,
+                    dropShadowColor: t.style?.dropShadowColor || "#000",
+                    dropShadowBlur: t.style?.dropShadowBlur || 4,
+                    dropShadowAngle: t.style?.dropShadowAngle || Math.PI / 6,
+                    dropShadowDistance: t.style?.dropShadowDistance || 10,
+                    wordWrap: t.style?.wordWrap || true,
+                    wordWrapWidth: t.style?.wordWrapWidth || 440,
+                  })
+                }
+                angle={t.rotate || 0}
+              />
+            ))}
+        </Stage>
 
         {showMakeVideo && (
           <div style={{ display: "flex", flexDirection: "column", width: 500 }}>

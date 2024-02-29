19e2cfded6d2f4c7002be1af0c2af5f8304a45a0
manvigautam09 <mnvsgautam9@gmail.com>
2024-03-01 00:09:36 +0530

    Add new types for video and slide interfaces

--- utils/types.ts
+++ utils/types.ts
@@ -8,6 +8,87 @@ export interface TextType {
   style?: ITextStyle;
 }
 
+type AspectRatio = "16:9 | 1:1 | 4:5 | 9:16";
+export interface TransitionInterface {
+  duration: number;
+  enter: "fade" | "slide" | "zoom" | "none"; // can be more according to what we decide
+  exit: "fade" | "slide" | "zoom" | "none";
+  direction: "left | right | up | down";
+}
+
+export interface TextLayerInterface {
+  type: "text";
+  id: string;
+  title: string;
+  rotate?: number;
+  position: {
+    x: number;
+    y: number;
+  };
+  transition?: TransitionInterface;
+  style?: ITextStyle;
+}
+
+export interface ImageLayerInterface {
+  type: "image";
+  id: string;
+  url: string;
+  rotate?: number;
+  position: {
+    x: number;
+    y: number;
+  };
+  style: {
+    width: number;
+    height: number;
+    objectFit: "cover" | "contain";
+  };
+}
+
+export interface StickerLayerInterface {
+  type: "sticker";
+  id: string;
+  url: string;
+  rotate?: number;
+  position: {
+    x: number;
+    y: number;
+  };
+  animation?: {
+    type: "bounce | shake | rotate | flip";
+    duration: number;
+  };
+}
+
+export type LayerType =
+  | TextLayerInterface
+  | ImageLayerInterface
+  | StickerLayerInterface;
+
+export interface AudioInterface {
+  url: string;
+  duration: number;
+  showCaptions: boolean;
+  captions: string;
+  volume: number;
+}
+
+export interface SlideInterface {
+  id: string;
+  audio?: AudioInterface;
+  transition?: TransitionInterface;
+  layers: LayerType[];
+}
+export interface VideoInterface {
+  id: string;
+  aspectRatio: AspectRatio;
+  thumbnail?: string;
+  slides: SlideInterface[];
+  audio?: AudioInterface;
+  transition?: TransitionInterface;
+}
+
+// Old structure
 export interface SlideType {
   type: "video" | "image" | "text" | "sticker"; // Type of the asset
   asset: string; // Reference to the asset
